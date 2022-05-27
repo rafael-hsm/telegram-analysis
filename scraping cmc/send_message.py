ec84b367e4715113fba0e909e7d0caadfe308761
Rafael Meireles <rafameireles2011@gmail.com>
2022-05-27 18:19:15 -0300

    update_telegram_crawler.

--- scraping cmc/send_message.py
+++ scraping cmc/send_message.py
@@ -1,27 +1,52 @@
+import configparser
+
 from telethon.sync import TelegramClient
 from telethon.tl.functions.messages import GetDialogsRequest
 from telethon.tl.types import InputPeerEmpty
 import csv
 
-api_id = 177
-api_hash = '5d2d28859'
-phone = '+62818'
-client = TelegramClient('test', api_id, api_hash)
+# Reading Configs
+# Access this for get our api_id and api_hash https://my.telegram.org/
+config = configparser.ConfigParser()
+config.read("../config.ini")
+
+# Setting configuration values
+api_id = config['Telegram']['api_id']
+api_hash = config['Telegram']['api_hash']
+
+api_hash = str(api_hash)
+
+phone = config['Telegram']['phone']
+username = config['Telegram']['username']
+
+client = TelegramClient(username, api_id, api_hash)
 
 client.connect()
 if not client.is_user_authorized():
     client.send_code_request(phone)
     client.sign_in(phone, input('Enter the code: '))
 
-
 from telethon.tl.functions.channels import JoinChannelRequest
 from telethon.tl.types import ChannelParticipantsAdmins
 
-target_groups = ['https://t.me/fimimarket', 'https://t.me/FimiAnnouncements', 'https://t.me/catcoiner', 'https://t.me/catcoiner', 'https://t.me/multiplanetaryinus', 'https://t.me/multiplanetaryinus', 'https://t.me/alyattesofficial', 'https://t.me/alyattes_tr', 'https://t.me/kinja_eth', 'https://t.me/kinja_eth', 'https://t.me/lasereyestoken', 'https://t.me/lasereyestoken', 'https://t.me/Chinazilla_official', 'https://t.me/Chinazilla_official', 'https://t.me/warenachat', 'https://t.me/warenaOfficial, https://www.instagram.com/warenaofficial/', 'https://t.me/KingdomKarnage', 'https://t.me/KingdomKarnage', 'https://t.me/partyboard_en', 'https://t.me/ApeFund', 'https://t.me/ApeFund', 'https://t.me/SHIBA2K22', 'https://t.me/SHIBA2K22', 'https://t.me/Chiba_inu1', 'https://t.me/Chiba_inu1', 'https://t.me/solventprotocol', 'https://t.me/SperaxUSD', 'https://t.me/realdogelana', 'https://t.me/SmartShibaOfficial', 'https://t.me/shibaxdefi', 'https://t.me/defibayofficial', 'https://t.me/defibayofficial', 'https://t.me/coinfreshofficial', 'https://t.me/coinfreshofficial', 'https://t.me/placewar', 'https://t.me/InuWarsLiquidation', 'https://t.me/InuWarsLiquidation', 'https://t.me/harmonyplayone', 'https://t.me/shibatoby', 'https://t.me/shibatoby']
+target_groups = ['https://t.me/fimimarket', 'https://t.me/FimiAnnouncements', 'https://t.me/catcoiner',
+                 'https://t.me/catcoiner', 'https://t.me/multiplanetaryinus', 'https://t.me/multiplanetaryinus',
+                 'https://t.me/alyattesofficial', 'https://t.me/alyattes_tr', 'https://t.me/kinja_eth',
+                 'https://t.me/kinja_eth', 'https://t.me/lasereyestoken', 'https://t.me/lasereyestoken',
+                 'https://t.me/Chinazilla_official', 'https://t.me/Chinazilla_official', 'https://t.me/warenachat',
+                 'https://t.me/warenaOfficial, https://www.instagram.com/warenaofficial/',
+                 'https://t.me/KingdomKarnage', 'https://t.me/KingdomKarnage', 'https://t.me/partyboard_en',
+                 'https://t.me/ApeFund', 'https://t.me/ApeFund', 'https://t.me/SHIBA2K22', 'https://t.me/SHIBA2K22',
+                 'https://t.me/Chiba_inu1', 'https://t.me/Chiba_inu1', 'https://t.me/solventprotocol',
+                 'https://t.me/SperaxUSD', 'https://t.me/realdogelana', 'https://t.me/SmartShibaOfficial',
+                 'https://t.me/shibaxdefi', 'https://t.me/defibayofficial', 'https://t.me/defibayofficial',
+                 'https://t.me/coinfreshofficial', 'https://t.me/coinfreshofficial', 'https://t.me/placewar',
+                 'https://t.me/InuWarsLiquidation', 'https://t.me/InuWarsLiquidation', 'https://t.me/harmonyplayone',
+                 'https://t.me/shibatoby', 'https://t.me/shibatoby']
 
 telegram_group_names = []
 for item in target_groups:
-    telegram_group_names.append(item.replace("https://t.me/",""))
+    telegram_group_names.append(item.replace("https://t.me/", ""))
 print(telegram_group_names)
 
 for target_group in telegram_group_names:
